ff9bf05b6b1ad0f01534e8f09cb6c2629a614c4c
PierreAlainC <pierrea.cypres@gmail.com>
2026-03-31 10:38:06 +0200

    Service + Controlleur + Bug fix faits

---
+++ src/Controller/Api/IssController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Service\IssService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+* @Route("api/v1/iss", name="api_iss") 
+*/
+class IssController extends AbstractController
+{
+    /**
+     * @Route("", name="show", methods={"GET"})
+     */
+    public function show(IssService $issService): JsonResponse
+    {
+        try {
+            return $this->json($issService->fetchCurrentIssPosition());
+        } catch (\Throwable $e) {
+            return $this->json([
+                /* $e->getMessage() */ 'error' => 'Il est impossible de récupérer la position de l\'ISS pour le moment, You can\'t catch me they say!',
+            ], 500);
+        }
+    }
+}
---
+++ src/Service/IssService.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class IssService
+{
+    private HttpClientInterface $client;
+    private string $apiUrl;
+
+    public function __construct(HttpClientInterface $client, string $issApiUrl)
+    {
+        $this->client = $client;
+        $this->apiUrl = $issApiUrl;
+    }
+
+    public function fetchCurrentIssPosition(): array
+    {           
+        // satellites/[id]
+        // Returns position, velocity, and other related information about a satellite 
+        // for a given point in time. 
+        // [id] is required and should be the NORAD catalog id. 
+        // For the ISS, that id is 25544.
+        // -> https://wheretheiss.at/w/developer
+        $response = $this->client->request(
+            'GET',
+            $this->apiUrl . '/satellites/25544'
+        );
+
+        $data = $response->toArray();
+
+        return [
+            'name' => $data['name'] ?? null,
+            'id' => $data['id'] ?? null,
+            'latitude' => $data['latitude'] ?? null,
+            'longitude' => $data['longitude'] ?? null,
+            'altitude' => $data['altitude'] ?? null,
+            'velocity' => $data['velocity'] ?? null,
+            'visibility' => $data['visibility'] ?? null,
+            'footprint' => $data['footprint'] ?? null,
+            'timestamp' => $data['timestamp'] ?? null,
+            'daynum' => $data['daynum'] ?? null,
+            'solar_lat' => $data['solar_lat'] ?? null,
+            'solar_lon' => $data['solar_lon'] ?? null,
+            'units' => $data['units'] ?? null,            
+        ];
+    }
+}
